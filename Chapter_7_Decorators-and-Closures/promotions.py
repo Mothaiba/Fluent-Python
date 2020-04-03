680285fe2a06dda75a1531cd4b072f64feaf0ae5
Tung Phung <tungphung2907@gmail.com>
2020-04-03 09:01:05 +0700

    add the first half of chapter 7

---
+++ Chapter_7_Decorators-and-Closures/promotions.py
@@ -0,0 +1,26 @@
+promos = []
+
+def promotion(promo):
+    promos.append(promo)
+    return promo
+
+@promotion
+def fidelity_promo(invoice):
+    '''customers with at least 1000 loyalty points are discounted for 5%'''
+    if invoice.customer.fidelity >= 1000:
+        return invoice.total() * 0.05
+    return 0
+
+@promotion
+def bulk_item_promo(invoice):
+    '''items with quantity >= 20 are discounted for 10%'''
+    discount = 0
+    for item in invoice.item_list:
+        if item.quantity >= 20:
+            discount += item.total() * 0.1
+    return discount
+
+@promotion
+def large_order_promo(invoice):
+    '''invoices with >= 10 different items are discounted for 7%'''
+    return invoice.total() * 0.07 if len(invoice.item_list) >= 10 else 0
